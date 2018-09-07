aada32356a8caef50c2fee5a507b1b639e98a590
Casey Blamires <clblamires@gmail.com>
2018-09-06 19:57:53 -0700

    Updated templates for home and page

--- functions.php
+++ functions.php
@@ -1,5 +1,8 @@
 <?php
 
+// this is a child theme based on Talon, by aThemes
+// make sure you have it installed before attempting to use this
+
 add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 
 function enqueue_parent_styles() {
